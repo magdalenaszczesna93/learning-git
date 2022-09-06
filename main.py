9abbd6ed9713965518370666977c34007e1ee087
Magdalena Szczesna <magdalena.szczesna.ue@gmil.com>
2022-09-06 22:34:45 +0200

    add dictionary and loop

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 print("'Hiszpańska inwazja' to najlpszy skecz Monty Pythona")
 print("a język hiszpański podobno jest łatwy do nauki")
-print("to jest pierwsza wersja projektu")
+print("to jest druga wersja projektu")
+print("Lista zakupów")
+#Zadanie 1
+lista_dict = {"piekarnia":["chleb", "bułki", "pączek"], "warzywniak":["marchew", "seler", "rukola"]}
+rzeczy = lista_dict.values()
+for sklep in lista_dict:
+    print(f"Idę do {sklep.capitalize()} i kupuję: {lista_dict[sklep]}")
+#nie mogę sobie poradzić z tym, jak sformatować listę rzeczy
+for rzeczy in lista_dict:
+    number = (len(lista_dict[rzeczy]))
+    print(type(number))
+#nie wiem czemu liczy tylko dla jednego skelpu
+    print(f"W sumie kupuję {number} produktów")
+print("\n")
